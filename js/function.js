16058ef25f6841dd63e1bbe5e70b6623bbb08163
ht7200 <251741906@qq.com>
2016-04-25 11:30:28 +0800

    animate

--- js/function.js
+++ js/function.js
@@ -1,35 +1,48 @@
 $(document).ready(function() {
 
-/******************QQ客服*************************/
+    /******************QQ客服*************************/
 
-	
-    $("#callSun").click(function() { 
-    	document.getElementById("qq_frame").src="http://wpa.qq.com/msgrd?v=3&uin=1379558294&site=qq&menu=yes";
+
+    $("#callSun").click(function() {
+        document.getElementById("qq_frame").src = "http://wpa.qq.com/msgrd?v=3&uin=1379558294&site=qq&menu=yes";
     })
 
     $("#callTing").click(function() {
-        document.getElementById("qq_frame").src="http://wpa.qq.com/msgrd?v=3&uin=976994550&site=qq&menu=yes";
+        document.getElementById("qq_frame").src = "http://wpa.qq.com/msgrd?v=3&uin=976994550&site=qq&menu=yes";
     })
 
 
-/******************图片轮播*************************/
+    /******************图片轮播*************************/
 
     var i = 0; //第i张图片
+    var inter //图片滚动定时器
     var size = $(".banner .imgs li").size();
     $(".banner .imgs li").first().addClass("on");
+  
+  	 bannerPlay();//轮播开始
 
     /*自动轮播*/
-    var t = setInterval(function() {
-        i++;
-        move()
-    }, 4000)
+    function bannerPlay() {
+      inter = setInterval(function() {
+            i++;
+            move()
+        }, 5000);
+    }
 
+   /*自动轮播*/
 
+        // inter = setInterval(function() {
+        //     i++;
+        //     move()
+        // }, 5000);
+  
 
     /*向左的按钮*/
     $(".banner .btn_l").click(function() {
         i--;
         move();
+        window.clearInterval(inter);
+        bannerPlay();
     })
 
 
@@ -37,7 +50,9 @@ $(document).ready(function() {
     /*向右的按钮*/
     $(".banner .btn_r").click(function() {
         i++;
-        move()
+        move();
+        window.clearInterval(inter);
+        bannerPlay();
     })
 
 
@@ -46,27 +61,28 @@ $(document).ready(function() {
 
         if (i == size) {
             i = 0;
-            $(".banner  .imgs li").eq(i).addClass("on").siblings().removeClass("on");
-            $(".banner .imgs li").eq(i).css({ opacity: 0.1 }).animate({ opacity: 1 }, 600)
+            $(".banner  .imgs li").eq(i).animate({ opacity: 1 }, 1000).siblings().animate({ opacity: 0 }, 1500);
+        //    $(".banner  .imgs li").eq(i).addClass("on").siblings().removeClass("on");
         } else if (i == -1) {
             i = size;
-            $(".banner .imgs li").eq(i).addClass("on").siblings().removeClass("on");
-            $(".banner .imgs li").eq(i).css({ opacity: 0.1 }).animate({ opacity: 1 }, 600)
+             $(".banner  .imgs li").eq(i).animate({ opacity: 1 }, 1000).siblings().animate({ opacity: 0 }, 1500);
+        //    $(".banner .imgs li").eq(i).addClass("on").siblings().removeClass("on");
+    
         } else {
-            $(".banner .imgs li").eq(i).addClass("on").siblings().removeClass("on");
-            $(".banner .imgs li").eq(i).css({ opacity: 0.1 }).animate({ opacity: 1 }, 600)
+        	 $(".banner  .imgs li").eq(i).animate({ opacity: 1 }, 1000).siblings().animate({ opacity: 0 }, 1500);
+        //    $(".banner .imgs li").eq(i).addClass("on").siblings().removeClass("on");
         }
         //	$(".banner .imgs").stop().animate({left:-i*liLenth},600)	
     }
 
 
-/******************文章切换*************************/
-	 $(".tabTable .tabBtn").first().addClass("on");
+    /******************文章切换*************************/
+    $(".tabTable .tabBtn").first().addClass("on");
 
-	 $(".tabTable .tabBtn").click(function() {
+    $(".tabTable .tabBtn").click(function() {
 
         $(this).addClass("on").siblings().removeClass("on");
-        var index =$(this).index();
+        var index = $(this).index();
         $(".article article").eq(index).addClass("on").siblings().removeClass("on");
     })
 
